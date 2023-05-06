2f9a5f8cb33619a6ea69c366d720ff951b73b7e2
Gz <354300537@qq.com>
2023-05-06 16:07:34 +0800

    完善vitest测试

--- test/$.test.ts
+++ test/$.test.ts
@@ -1,12 +1,13 @@
+import os from "node:os";
 import { describe, expect, test, vi } from "vitest"
 import {$} from '../src/core'
 
 describe("core", () => {
-  test('是否正确执行脚本', async () => {
+  test('是否正确执行脚本--windows', async () => {
     const EnvValue = 'test_name'
     vi.stubEnv('GZTEST', EnvValue)
     const result = await $`echo %GZTEST%` // cmd
-    expect(result).toContain(EnvValue)
+    expect(result).toBe(EnvValue + os.EOL)
     vi.unstubAllEnvs()
   })
 })
