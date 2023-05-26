a0791536e5c0774f6150605398c2a9396e8648b0
Gz <354300537@qq.com>
2023-05-26 18:29:46 +0800

    实现创建文件夹

--- src/core.ts
+++ src/core.ts
@@ -1,27 +1,65 @@
+import which from 'which'
 import { spawn } from 'node:child_process'
 
-export async function $(command: TemplateStringsArray) {
+const defaultOptions: { shell: boolean | string; preFix: string } = {
+  shell: true,
+  preFix: 'set -euo pipefail;',
+}
+
+try {
+  defaultOptions.shell = which.sync('bash')
+} catch (error) {
+  if (process.platform === 'win32') {
+    defaultOptions.shell = which.sync('powershell.exe')
+  }
+}
+
+export async function $(command: TemplateStringsArray, ...values: string[]) {
   return new Promise((r, j) => {
     console.log(`command`, command)
+    console.log(`values`, values)
     const commandStr = command[0]
 
-    let result: Array<Uint8Array>  = []
-    let size = 0;
-    const ls = spawn(commandStr, {shell: true})
+    let result: Array<Uint8Array> = []
+    let size = 0
+    const cmd = `${defaultOptions.preFix} ${commandStr} '${values[0]}'`
+    const ls = spawn(cmd, {
+      shell: defaultOptions.shell,
+      stdio: ['inherit', 'pipe', 'pipe'],
+    })
     ls.stdout.on('data', (data) => {
+      log({ data, type: 'stdout' })
       result.push(data)
-      size += data.length;
+      size += data.length
       // console.log(`stdout: ${data}`)
     })
-  
+
     ls.stderr.on('data', (data) => {
-      console.error(`stderr: ${data}`)
+      log({ data, type: 'stderr' })
+      j('翻车')
     })
-  
+
     ls.on('close', (code) => {
       r(Buffer.concat(result, size).toString())
       console.log(`child process exited with code ${code}`)
     })
-    
   })
+}
+
+type logEntry = {
+  type: 'stdout' | 'stderr'
+  data: Buffer
+}
+function log(entry: logEntry) {
+  switch (entry.type) {
+    case 'stderr':
+      process.stderr.write(entry.data)
+      break
+    case 'stdout':
+      process.stdout.write(entry.data)
+      break
+
+    default:
+      break
+  }
 }
--- test/$.test.ts
+++ test/$.test.ts
@@ -1,13 +1,38 @@
-import os from "node:os";
 import { describe, expect, test, vi } from "vitest"
 import {$} from '../src/core'
+import { existsSync, rmdirSync } from 'node:fs';
+
 
 describe("core", () => {
-  test('是否正确执行脚本--windows', async () => {
+  test('是否正确执行脚本', async () => {
     const EnvValue = 'test_name'
     vi.stubEnv('GZTEST', EnvValue)
-    const result = await $`echo %GZTEST%` // cmd
-    expect(result).toBe(EnvValue + os.EOL)
+    const result = await $`echo $GZTEST` // cmd
+    expect(result).toContain(EnvValue) // bash
     vi.unstubAllEnvs()
+  })
+  test('是否正确创建文件夹', async () => {
+    expect.assertions(1)
+    const testPath = `./gz_command_test`
+    if (existsSync(testPath)) {
+      return
+    }
+    const result = await $`mkdir ${testPath}` // cmd
+    if (existsSync(testPath)) {
+      rmdirSync(testPath)
+      expect(result).toBe('')
+    }
+  })
+  test('是否正确创建文件夹 far & bar', async () => {
+    expect.assertions(1)
+    const testPath = `./far & bar`
+    if (existsSync(testPath)) {
+      return
+    }
+    const result = await $`mkdir ${testPath}` // cmd
+    if (existsSync(testPath)) {
+      rmdirSync(testPath)
+      expect(result).toBe('')
+    }
   })
 })
